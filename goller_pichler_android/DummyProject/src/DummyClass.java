7fc94ba699711e112ff656c244edce95cb6425df
Manuel Froeschl <froeschl.man@gmail.com>
2021-05-15 18:41:26 +0200

    Now finally i destroyed everything, greets, JoH<3

--- goller_pichler_android/DummyProject/src/DummyClass.java
+++ goller_pichler_android/DummyProject/src/DummyClass.java
@@ -4,24 +4,25 @@ public class DummyClass {
 	public void existing(){
 		System.out.print("Existing Method");
 		System.out.println("Hallo Lukas");
+		System.out.println("Ye just pls work");
 		//new LoC
 		int newLineOfCode;
 	}
 	
-	public void thisIsANewMethod() { 
-		int counter = 0;
-		for (int i=0;i<=10;i++) {
-			System.out.println(i);
-
-			System.out.println("Hallo");
-
-			System.out.println(i);
-			
-
-			//new LoC
-			int anotherNewLineOfCode;
-
-			System.out.println("fixes the bug");
+//	public void thisIsANewMethod() { 
+//		int counter = 0;
+//		for (int i=0;i<=10;i++) {
+//			System.out.println(i);
+//
+//			System.out.println("Hallo");
+//
+//			System.out.println(i);
+//			
+//
+//			//new LoC
+//			int anotherNewLineOfCode;
+//
+//			System.out.println("fixes the bug");
 
 		}
 	}
